f53523425584d47e7a9432d9bb1c5e35c65be6e4
rudnytskyiak24-bit <rudnytskyi_ak24@nuwm.edu.ua>
2025-10-07 19:25:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,97 @@
-﻿using System;
+using System;
 
-namespace LabWork
+namespace StudentDecoratorExample
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    // Базовий інтерфейс студента
+    public interface IStudent
+    {
+        string Name { get; }
+        void DisplayInfo();
+    }
+
+    // Конкретний студент
+    public class Student : IStudent
+    {
+        public string Name { get; private set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Студент: {Name}");
+        }
+    }
+
+    // Абстрактний декоратор
+    public abstract class StudentDecorator : IStudent
+    {
+        protected IStudent _student;
+
+        public StudentDecorator(IStudent student)
+        {
+            _student = student;
+        }
+
+        public string Name => _student.Name;
+
+        public virtual void DisplayInfo()
+        {
+            _student.DisplayInfo();
+        }
+    }
+
+    // Декоратор для додавання стипендії
+    public class ScholarshipDecorator : StudentDecorator
+    {
+        public double ScholarshipAmount { get; private set; }
+
+        public ScholarshipDecorator(IStudent student, double scholarshipAmount)
+            : base(student)
+        {
+            ScholarshipAmount = scholarshipAmount;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Стипендія: {ScholarshipAmount} грн");
+        }
+    }
+
+    // Декоратор для додавання балу успішності
+    public class GPAStudentDecorator : StudentDecorator
+    {
+        public double GPA { get; private set; }
+
+        public GPAStudentDecorator(IStudent student, double gpa)
+            : base(student)
+        {
+            GPA = gpa;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Бал успішності: {GPA}");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            IStudent student = new Student("Іван Іванов");
+
+            // Декоруємо студента додатковими характеристиками
+            student = new ScholarshipDecorator(student, 1500);
+            student = new GPAStudentDecorator(student, 4.5);
+
+            student.DisplayInfo();
+
+            Console.ReadLine();
         }
     }
 }
